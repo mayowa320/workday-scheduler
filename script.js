83192ff29bf8a24262f3899396b40df4cae72af1
Mayowa <mayowa320@gmail.com>
2022-07-07 15:37:40 -0400

    added local storage and styles to page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let schedule = {
   9: "",
-  10: "",
+  10: "hiiiii",
   11: "",
   12: "",
   13: "",
@@ -9,13 +9,29 @@ let schedule = {
   16: "",
   17: "",
 };
+let storedSchedule = localStorage.getItem("schedule");
+if (storedSchedule) {
+  schedule = JSON.parse(storedSchedule);
+}
+let time = new Date();
+let currentHour = time.getHours();
 $.each(schedule, function (key, value) {
   let row = $("<div>").addClass("row");
   let hour = $("<div>").addClass("hour").text(key);
-  let textarea = $("<textarea>");
+  let textarea = $("<textarea>")
+    .val(value)
+    .on("input", function (event) {
+      schedule[key] = this.value;
+    })
+    .addClass(
+      currentHour > +key ? "past" : currentHour === +key ? "present" : "future"
+    );
   let saveBtn = $("<button>")
     .addClass("saveBtn")
-    .html('<i class="fa-solid fa-floppy-disk"></i>');
+    .html('<i class="fa fa-save"></i>')
+    .on("click", function () {
+      localStorage.setItem("schedule", JSON.stringify(schedule));
+    });
   row.append(hour, textarea, saveBtn);
   $(".container").append(row);
 });
